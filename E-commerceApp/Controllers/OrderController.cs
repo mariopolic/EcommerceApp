9dd0cd5117e4d2b5bacad49c1b928af31aece53b
Mario Polic <mpolic@evoila.com>
2023-05-31 14:26:29 +0200

    MP - Added order controller

---
+++ E-commerceApp/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using ECA.ViewModels.RequestModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerceApp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public  IActionResult GetAllOrders()
+        {
+            return Ok();
+        }
+        [HttpGet("get/{orderId}")]
+        public IActionResult GetSingleOrder(int orderId)
+        {
+            return Ok();
+        }
+        [HttpPost("add/{customerId}")]
+        public IActionResult AddOrder(int customerId)
+           => Ok();
+
+        [HttpPut("update/{OrderId}")]
+        public  IActionResult UpdateOrder(int OrderId, [FromBody] ProductRequestModel productRequest)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("delete/{OrderId}")]
+        public  IActionResult DeleteOrder(int OrderId)
+        {
+            return Ok(OrderId);
+        }
+    }
+}
