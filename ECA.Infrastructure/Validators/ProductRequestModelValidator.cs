977ca245b9ad17fd62262e55ce5e2b1b07c77590
Mario Polic <mpolic@evoila.com>
2023-07-05 14:18:42 +0200

    MP - added new validaton rules for products

--- ECA.Infrastructure/Validators/ProductRequestModelValidator.cs
+++ ECA.Infrastructure/Validators/ProductRequestModelValidator.cs
@@ -14,6 +14,10 @@ namespace ECA.Infrastructure.Validators
         {
             RuleFor(x => x.ProductName).NotEmpty().WithMessage("Product Name can not be empty");
             RuleFor(x => x.ProductName).NotEqual("string").WithMessage("Product Name can not be string");
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("Product Description can not be empty");
+            RuleFor(x => x.ProductDescription).NotEqual("string").WithMessage("Product description can not be string");
+            RuleFor(x => x.ProductPrice).NotEmpty().WithMessage("Price can not be empty");
+            RuleFor(x => x.ProductPrice).NotEqual(0).WithMessage("Product price can not be 0");
         }
     }
 }
