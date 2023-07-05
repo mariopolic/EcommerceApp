f2f3ba64aa098bbe5a8d02ce19362259be316467
Mario Polic <mpolic@evoila.com>
2023-07-05 09:19:34 +0200

    MP - added fluent api

---
+++ ECA.Infrastructure/Validators/CustomerRequestModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using ECA.ViewModels.ViewModels;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECA.Infrastructure.Validators
+{
+    public class CustomerRequestModelValidator:AbstractValidator<CustomerRequestModel>
+    {
+
+    }
+}
