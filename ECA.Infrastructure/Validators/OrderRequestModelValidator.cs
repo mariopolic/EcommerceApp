e3d8048d87cbd77937627e19f7f691d0e7b66584
Mario Polic <mpolic@evoila.com>
2023-07-05 11:22:52 +0200

    MP - added new rules for Order

---
+++ ECA.Infrastructure/Validators/OrderRequestModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using ECA.ViewModels.RequestModel;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECA.Infrastructure.Validators
+{
+    public class OrderRequestModelValidator:AbstractValidator<OrderRequestModel>
+    {
+        public OrderRequestModelValidator()
+        {
+            RuleFor(x => x.customerId).NotEmpty().WithMessage("customerId can not be null");
+        }
+    }
+}
